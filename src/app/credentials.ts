2f566b2cac7c2de8674a300c4d8c0bc2f8447d97
mkobar <mike@exlorev.com>
2018-08-16 11:28:05 -0400

    point to essr-aoc

---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDV-2PsvBDQI_0bwTjT9yowIhE1gv05CO8",
+  authDomain: "esrr-aoc.firebaseapp.com",
+  databaseURL: "https://esrr-aoc.firebaseio.com",
+  projectId: "esrr-aoc",
+  storageBucket: "esrr-aoc.appspot.com",
+  messagingSenderId: "975907153175"
+};
