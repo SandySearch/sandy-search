fd79b1ce81817fad9ae529a8d5065014919bad46
mkobar <mike@exlorev.com>
2018-08-17 12:19:56 -0400

    init

---
+++ src/providers/entry/entry.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import {
+  AngularFireDatabase,
+  AngularFireObject,
+  AngularFireList
+} from 'angularfire2/database';
+
+import firebase from 'firebase/app';
+
+@Injectable()
+export class EntryProvider {
+  public entryList: AngularFireList<any>;
+  public userId: string;
+  constructor(
+    public afAuth: AngularFireAuth,
+    public afDatabase: AngularFireDatabase
+  ) {
+    this.afAuth.authState.subscribe(user => {
+      this.userId = user.uid;
+      this.entryList = this.afDatabase.list(`/userProfile/${user.uid}/billList`);
+    });
+  }
+
+  getEntryList(): AngularFireList<any> {
+    return this.entryList;
+  }
+
+  getEntry(entryId: string): AngularFireObject<any> {
+    return this.afDatabase.object(
+      `/userProfile/${this.userId}/billList/${entryId}`
+    );
+  }
+
+  createEntry(
+    name: string,
+    amount: number,
+    dueDate: string = null,
+    paid: boolean = false
+  ): Promise<any> {
+    const newEntryRef: firebase.database.ThenableReference = this.entryList.push(
+      {}
+    );
+    return newEntryRef.set({
+      name,
+      amount,
+      dueDate,
+      paid,
+      id: newEntryRef.key
+    });
+  }
+
+  removeEntry(entryId: string): Promise<any> {
+    return this.entryList.remove(entryId);
+  }
+
+  payEntry(entryId: string): Promise<any> {
+    return this.entryList.update(entryId, { paid: true });
+  }
+
+/***
+  takeBillPhoto(billId: string, imageURL: string): AngularFireUploadTask {
+    const storageRef: AngularFireStorageReference = this.afStorage.ref(
+      `${this.userId}/${entryId}/billPicture/`
+    );
+
+    return storageRef.putString(imageURL, 'base64', {
+      contentType: 'image/png'
+    });
+  }
+  ***/
+
+  storeDownloadUrl(entryId: string, downloadUrl: string): Promise<any> {
+    console.log(entryId, downloadUrl);
+    return this.entryList.update(entryId, { picture: downloadUrl });
+  }
+
+}
