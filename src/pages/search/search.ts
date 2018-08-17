973b610cf9ffb9d9432e4b36f240bf83d791a5f8
mkobar <mike@exlorev.com>
2018-08-17 13:47:55 -0400

    added search page

---
+++ src/pages/search/search.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import {
+  IonicPage,
+  NavController,
+  Loading,
+  LoadingController
+} from 'ionic-angular';
+import { OldHomePage } from '../oldhome/oldhome';
+
+/**
+ * Generated class for the SearchPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-search',
+  templateUrl: 'search.html',
+})
+export class SearchPage {
+
+  constructor(
+    public navCtrl: NavController,
+    public loadingCtrl: LoadingController
+  ) { }
+
+  listESNY(): void { this.navCtrl.setRoot(OldHomePage); }
+
+  listEFW(): void { this.navCtrl.push('LandingPage'); }
+
+  listGS(): void { this.navCtrl.push('OldHomePage'); }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SearchPage');
+  }
+
+}
