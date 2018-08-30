64cd3b69552dce1404005ab7ea98882f0cd3ddbe
mkobar <mike@exlorev.com>
2018-08-30 16:41:18 -0400

    getting geohash on map

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -18,14 +18,17 @@ import { GeoProvider } from '../../providers/geo/geo'        // eslint-disable-l
     .sebm-google-map-container {
        height: 300px;
      }
+     .agm-map {
+       height: 300px;
+     }
   `],
-  //styleUrls: ['./map.scss']
   //styleUrls: ['./map.scss']
 })
 //export class MapPage implements OnInit {
 export class MapPage implements OnDestroy {
-    lat: number;
-    lng: number;
+    lat: number
+    lng: number
+    rangeKm: number = 25
 
     markers: any;
     subscription: any;
@@ -50,19 +53,21 @@ export class MapPage implements OnDestroy {
     }
   
     private getUserLocation() {
-     /// locate the user
+     // locate the user
      if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
          this.lat = position.coords.latitude
          this.lng = position.coords.longitude
          console.log("this.lat/this.lng 1= "+this.lat+'/'+this.lng)
-         this.geo.getLocations(100, [this.lat, this.lng])
+         // get service locations
+         this.geo.getLocations(this.rangeKm, [this.lat, this.lng])
        });
      } else {
        this.lat = 51.678418
        this.lng = -73.809007
        console.log("this.lat/this.lng 2= "+this.lat+'/'+this.lng)
-       this.geo.getLocations(100, [this.lat, this.lng])
+       // get service locations
+       this.geo.getLocations(this.rangeKm, [this.lat, this.lng])
      }
      //console.log("this.lat/this.lng = "+this.lat+'/'+this.lng)
      //this.geo.getLocations(100, [this.lat, this.lng])
